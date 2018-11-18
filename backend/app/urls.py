52db5ad109aad841c6ab155642d788d233fb4d50
Fernando Felix <fernandojr.ifcg@live.com>
2018-11-18 17:46:02 -0300

    Fix lint

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from app import views
 
-router = routers.DefaultRouter()
+# pylint: disable=C0103
+router = routers.DefaultRouter() # noqa
 router.register(r'users', views.UserViewSet)
 router.register(r'product', views.ProductViewSet)
 router.register(r'order', views.OrderViewSet)
--- backend/app/views.py
+++ backend/app/views.py
@@ -14,6 +14,7 @@ class UserViewSet(viewsets.ModelViewSet):
 
 
 class ProductViewSet(viewsets.ModelViewSet):
+    # pylint: disable=C0301
     """
     API endpoint that allows products to be viewed or edited.
 
@@ -23,7 +24,7 @@ class ProductViewSet(viewsets.ModelViewSet):
             "current_price": 1.0,
             "image_url": "https://m.media-amazon.com/images/M/MV5BNzQzMzJhZTEtOWM4NS00MTdhLTg0YjgtMjM4MDRkZjUwZDBlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
         }
-    """
+    """ # noqa
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
